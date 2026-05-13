dd1feff39d4ecc463e61008644087e23bef6c015
Mounir Benberkan <124848349+moe55g@users.noreply.github.com>
2026-05-14 00:46:29 +0200

    Add project files

---
+++ src/stack.js
@@ -0,0 +1,17 @@
+const _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+exports.peek = function () {
+    return _.last(stack);
+}
